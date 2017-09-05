4301a8f4eba98542561e69009d7b8ceff3f59a79
smoczyna <smoczyna@laptok-smoka>
2017-09-05 22:36:33 +0100

    Revert "job test pre-ready, complaining for hibernate connection issue shout be replaced with mocked database source"
    This reverts commit 70d787d2cd3e9bca7eec3911743638d00b501167.

--- src/test/java/com/vzw/booking/ms/batch/jobs/DatabaseToCsvFileJobLauncherTest.java
+++ src/test/java/com/vzw/booking/ms/batch/jobs/DatabaseToCsvFileJobLauncherTest.java
@@ -5,10 +5,9 @@
  */
 package com.vzw.booking.ms.batch.jobs;
 
-import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.context.annotation.PropertySource;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
@@ -16,16 +15,27 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @author smorcja
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@PropertySource("classpath:application.properties")
+//@SpringApplicationConfiguration(classes = SpringBatchTestApplication.class)
 public class DatabaseToCsvFileJobLauncherTest {
-
+    
+    public DatabaseToCsvFileJobLauncherTest() {
+    }
+    
+//    private JdbcTemplate jdbcTemplate;
+//
+//    @Autowired
+//    public void setDataSource(DataSource dataSource) {
+//        this.jdbcTemplate = new JdbcTemplate(dataSource);
+//    }
+    
     @Test
     public void launchJob() throws Exception {
-//        ApplicationContext context = new AnnotationConfigApplicationContext(SpringBatchTestApplication.class, DatabasesConfig.class, DatabaseToCsvFileJobConfig.class);
-//        JobLauncherTestUtils jobLauncherTestUtils = context.getBean(JobLauncherTestUtils.class);
+        
+            
 //        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
 //        Assert.assertEquals(jobExecution.getStatus(), BatchStatus.COMPLETED);
-        assertTrue(true);
-    }
 
+    
+    }
+    
 }
