e11d8d0ea6dce91525e02a7ac0381e068e2f9504
smorcja <smorcja@IIE00166L171706.emea.dsmain.com>
2017-09-20 11:45:32 +0100

    dynamically scheduled jobs

--- src/main/java/eu/squadd/batch/launchers/DynamicallyScheduledJobLauncher.java
+++ src/main/java/eu/squadd/batch/launchers/DynamicallyScheduledJobLauncher.java
@@ -2,8 +2,6 @@ package eu.squadd.batch.launchers;
 
 import eu.squadd.batch.jobs.DynaScheduleJob;
 import java.util.ArrayList;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +9,10 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.concurrent.ScheduledFuture;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.Trigger;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.support.CronTrigger;
 
 /**
@@ -23,22 +22,23 @@ import org.springframework.scheduling.support.CronTrigger;
 @Component
 public class DynamicallyScheduledJobLauncher {
 
-    private final JobLauncher jobLauncher;
-    private final ThreadPoolTaskScheduler  threadPoolTaskScheduler;
+    private final JobLauncher jobLauncher;    
     private final DynaScheduleJob jobRunner;
     private final List<ScheduledFuture> scheduledTasks;
 
-    @Value("${csv.to.database.job.cron}")
+    @Value("${csv.to.database.job.cron}") // this doesn't work 
     private String cronExpression;
     
     @Autowired
-    public DynamicallyScheduledJobLauncher(JobLauncher jobLauncher, Job jobToBeExecuted, ThreadPoolTaskScheduler  threadPoolTaskScheduler, DynaScheduleJob jobRunner) throws Exception {
+    public DynamicallyScheduledJobLauncher(JobLauncher jobLauncher, @Qualifier("billedBookingAggregateJob") Job jobToBeExecuted, TaskScheduler jobScheduler, DynaScheduleJob jobRunner) throws Exception {
         this.jobLauncher = jobLauncher;
-        this.threadPoolTaskScheduler = threadPoolTaskScheduler;
         this.jobRunner = jobRunner;
-        Trigger trigger = new CronTrigger(cronExpression);
+        Trigger trigger = new CronTrigger("0 * * * * *");
         this.jobRunner.setJob(jobToBeExecuted);
         this.scheduledTasks = new ArrayList();
-        this.scheduledTasks.add(this.threadPoolTaskScheduler.schedule((Runnable) jobRunner, trigger));
+        this.scheduledTasks.add(jobScheduler.schedule((Runnable) jobRunner, trigger)); 
+        
+        // scheduler works but the job is not properly configured    
+        // No job configuration with the name [billedBookingAggregateJob] was registered 
     }
 }
