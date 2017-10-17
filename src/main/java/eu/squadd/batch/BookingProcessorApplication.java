914f75696ddf4a7b1c0907d576f75f77dd0a836f
smorcja <smorcja@IIE00166L171706.emea.dsmain.com>
2017-10-17 13:13:04 +0100

    different logger tries

--- src/main/java/eu/squadd/batch/BookingProcessorApplication.java
+++ src/main/java/eu/squadd/batch/BookingProcessorApplication.java
@@ -20,14 +20,26 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 public class BookingProcessorApplication {
 
-	/**
-	 * This the main method for the application. 
-         * It executes the application
-	 * 
-	 * @param args - arguments passed to the spring boot application.
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(BookingProcessorApplication.class, args);
-	}
-
+    //static Logger LOGGER = Logger.getLogger(BookingProcessorApplication.class.getName());
+    
+    /**
+     * This the main method for the application. It executes the application
+     *
+     * @param args - arguments passed to the spring boot application.
+     */
+    public static void main(String[] args) {
+//        try {
+//            FileHandler fh = new FileHandler("c:/Users/smorcja/Temp/tmp/File.log");
+//            LOGGER.addHandler(fh);
+//            SimpleFormatter formatter = new SimpleFormatter();
+//            fh.setFormatter(formatter);
+//            
+//            // the following statement is used to log any messages
+//            LOGGER.info("Booking Wholesale App: ");
+//            
+//        } catch (IOException | SecurityException ex) {
+//            Logger.getLogger(BookingWholesaleApplication.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        SpringApplication.run(BookingProcessorApplication.class, args);
+    }
 }
